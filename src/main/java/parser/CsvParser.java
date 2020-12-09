e1c1f2f88baf5a3cdf63c7cad9d54cf147004855
Patrick Steiger <patrick.steiger@uni-ulm.de>
2020-12-09 12:58:50 +0100

    Add CsvParser

---
+++ src/main/java/parser/CsvParser.java
@@ -0,0 +1,39 @@
+package parser;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.HeaderColumnNameMappingStrategy;
+import interfaces.Datablock;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class CsvParser {
+    public static final char COLUMN_SEPARATOR = '$';
+
+    /**
+     * Parses Datablocks from given CSV-File.
+     * @param csvFilePath Path to CSV-File.
+     * @param datablockClass Class containing the schema to parse.
+     * @param <T> The type needs to be a Datablock.
+     * @return List of parsed Datablocks
+     * @throws IOException In case of error while working with the file.
+     * @throws IllegalStateException In case of error while parsing CSV.
+     */
+    public static <T extends Datablock> List<T> parseDatablocks(String csvFilePath, Class<T> datablockClass) throws IOException {
+        BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(csvFilePath));
+        HeaderColumnNameMappingStrategy<T> headerStrategy = new HeaderColumnNameMappingStrategy<>();
+        headerStrategy.setType(datablockClass);
+        CsvToBean<T> beanReader = new CsvToBeanBuilder<T>(bufferedReader)
+                .withMappingStrategy(headerStrategy)
+                .withSeparator(COLUMN_SEPARATOR)
+                .withIgnoreEmptyLine(true)
+                .build();
+        List<T> datablocks = beanReader.parse();
+        bufferedReader.close();
+        return datablocks;
+    }
+}
