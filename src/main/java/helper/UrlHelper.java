6ebb59fd092b21661d14cd49099eadb23c263dd7
Patrick Steiger <patrick.steiger@uni-ulm.de>
2020-12-13 18:28:28 +0100

    Add URLs for BodySite and Extensions

--- src/main/java/helper/UrlHelper.java
+++ src/main/java/helper/UrlHelper.java
@@ -6,4 +6,9 @@ public class UrlHelper {
     public static final String SNOMED_CLINICAL_TERMS = "http://snomed.info/sct";
     public static final String OPS_DIMDI_SYSTEM = "http://fhir.de/CodeSystem/dimdi/ops";
     public static final String OPS_SEITENLOKALISATION = "http://fhir.de/StructureDefinition/seitenlokalisation";
+
+    public static final String BODYSITE_URL = "http://hl7.org/fhir/ValueSet/body-site";
+
+    public static final String RECORDED_DATE_URL = "https://www.medizininformatik-initiative.de/fhir/core/modul-prozedur/StructureDefinition/procedure-recordedDate";
+    public static final String DURCHFUEHRUNGSABSICHT_URL = "https://www.medizininformatik-initiative.de/fhir/core/modul-prozedur/StructureDefinition/Durchfuehrungsabsicht";
 }
