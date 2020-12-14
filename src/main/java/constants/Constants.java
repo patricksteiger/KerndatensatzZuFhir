7c7e840424039191608ac5bbefeca1a09da50a16
Patrick Steiger <patrick.steiger@uni-ulm.de>
2020-12-14 20:08:19 +0100

    Add constants for subject

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -3,4 +3,7 @@ package constants;
 public class Constants {
     public static final String EMPTY_DISPLAY = "";
     public static final String VERSION_2020 = "2020";
+
+    public static final String SOURCE_UKU_SAP = "UKU-SAP-";
+    public static final String SOURCE_UKU_SAP_PROZEDUR = SOURCE_UKU_SAP + "Prozedur-Procedure";
 }
