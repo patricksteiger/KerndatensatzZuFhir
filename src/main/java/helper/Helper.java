8f715628cb2f10fb2684a3b6fe894b32281d23f4
Patrick Steiger <patrick.steiger@uni-ulm.de>
2020-12-14 20:06:18 +0100

    Change month to be 0-indexed

--- src/main/java/helper/Helper.java
+++ src/main/java/helper/Helper.java
@@ -8,7 +8,8 @@ public class Helper {
         Calendar calendar = Calendar.getInstance();
         String[] dates = germanDate.split("\\.");
         calendar.set(Calendar.DAY_OF_MONTH, Integer.parseInt(dates[0]));
-        calendar.set(Calendar.MONTH, Integer.parseInt(dates[1]));
+        // Months are 0-indexed.
+        calendar.set(Calendar.MONTH, Integer.parseInt(dates[1]) - 1);
         calendar.set(Calendar.YEAR, Integer.parseInt(dates[2]));
         return calendar.getTime();
     }
