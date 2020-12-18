8f26213caf2b50daf9995de868acd96eda3bc1d5
Patrick Steiger <patrick.steiger@uni-ulm.de>
2020-12-18 14:15:34 +0100

    Add constant for procedure id

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -4,6 +4,8 @@ public class Constants {
     public static final String EMPTY_DISPLAY = "";
     public static final String VERSION_2020 = "2020";
 
-    public static final String SOURCE_UKU_SAP = "UKU-SAP-";
-    public static final String SOURCE_UKU_SAP_PROZEDUR = SOURCE_UKU_SAP + "Prozedur-Procedure";
+    public static final String UKU = "UKU";
+    public static final String SOURCE_UKU_SAP = UKU + "-SAP";
+    public static final String SOURCE_UKU_SAP_PROZEDUR = SOURCE_UKU_SAP + "-Prozedur-Procedure";
+    public static final String UKU_FHIR_PROCEDURE = "UKU_Procedure_1.0.0.0";
 }
