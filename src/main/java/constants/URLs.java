0208b00b3c1c8b505ff407e4f9f426c69eac6dde
Patrick Steiger <patrick.steiger@uni-ulm.de>
2020-12-18 19:34:11 +0100

    Add SeitenlokalisationCod enum

--- src/main/java/constants/URLs.java
+++ src/main/java/constants/URLs.java
@@ -6,7 +6,9 @@ public class URLs {
     public static final String SNOMED_CLINICAL_TERMS = "http://snomed.info/sct";
     public static final String OPS_DIMDI_SYSTEM = "http://fhir.de/CodeSystem/dimdi/ops";
     public static final String OPS_SEITENLOKALISATION = "http://fhir.de/StructureDefinition/seitenlokalisation";
-    public static final String OPS_SEITENLOKALISATION_OID = "urn:oid:1.2.276.0.76.11.123";
+    //public static final String OPS_SEITENLOKALISATION_OID = "urn:oid:1.2.276.0.76.11.123";
+    public static final String OPS_SEITENLOKALISATION_OID = "urn:oid:2.16.840.1.113883.3.7.1.7";
+    public static final String OPS_SEITENLOKALISATION_OID_ERROR = "http://terminology.hl7.org/CodeSystem/v3-NullFlavor";
 
     public static final String BODYSITE_URL = "http://hl7.org/fhir/ValueSet/body-site";
 
---
+++ src/main/java/enums/SeitenlokalisationCode.java
@@ -0,0 +1,54 @@
+package enums;
+
+import constants.URLs;
+
+import java.util.Arrays;
+
+public enum SeitenlokalisationCode {
+
+    RECHTS("R", "rechts", URLs.OPS_SEITENLOKALISATION_OID),
+    LINKS("L", "links", URLs.OPS_SEITENLOKALISATION_OID),
+    BEIDSEITS("B", "beidseits", URLs.OPS_SEITENLOKALISATION_OID),
+    MITTELLINIENZONE("M", "Mittellinienzone", URLs.OPS_SEITENLOKALISATION_OID),
+    NICHT_ZUTREFFEND("NA", "nicht zutreffend", URLs.OPS_SEITENLOKALISATION_OID_ERROR),
+    UNBEKANNT("UNK", "unbekannt", URLs.OPS_SEITENLOKALISATION_OID_ERROR);
+
+    private final String code;
+    private final String display;
+    private final String codeSystem;
+
+    SeitenlokalisationCode(String code, String display, String codeSystem) {
+        this.code = code;
+        this.display = display;
+        this.codeSystem = codeSystem;
+    }
+
+    /**
+     * Gets SeitenlokalisationsCode for given, case-sensitive code.
+     *
+     * @param code Valid codes: R, L, B, M, NA, UNK.
+     * @return SeitenlokalisationsCode
+     * @throws IllegalArgumentException If invalid code is given.
+     */
+    public static SeitenlokalisationCode getSeitenlokalisationByCode(String code) {
+        return Arrays.stream(SeitenlokalisationCode.values())
+                .filter(seitenEnum -> seitenEnum.getCode().equals(code))
+                .findFirst()
+                .orElseThrow(
+                        () -> new IllegalArgumentException("Code '" + code + "' is not a valid Seitenlokalisationscode")
+                );
+
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public String getDisplay() {
+        return this.display;
+    }
+
+    public String getCodeSystem() {
+        return this.codeSystem;
+    }
+}
