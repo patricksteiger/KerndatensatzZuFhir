0f8fcb697488db4a826eaba401824b42d5fa302b
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-02-02 20:45:51 +0100

    add tests for trimCharacter, trimQuotes, splitCode and extractCode

--- src/test/java/helper/HelperTest.java
+++ src/test/java/helper/HelperTest.java
@@ -5,11 +5,68 @@ import org.junit.jupiter.api.Test;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HelperTest {
+  @Test
+  public void testExtractCode() {
+    String code = "code=";
+    List<String> words = new ArrayList<>();
+    assertEquals("", Helper.extractCode(words, code));
+    words.add("BadStart");
+    assertEquals("", Helper.extractCode(words, code));
+    words.add("code=\"20570-8\"");
+    assertEquals("20570-8", Helper.extractCode(words, code));
+    words.add(0, "code=123");
+    assertEquals("123", Helper.extractCode(words, code));
+  }
+
+  @Test
+  public void testSplitCode() {
+    String nullStr = null;
+    assertEquals(0, Helper.splitCode(nullStr).size());
+    String emptyStr = "";
+    assertEquals(0, Helper.splitCode(emptyStr).size());
+    String codeSystem = "  system=\"http://loinc.org\"";
+    String codeCode = "code=\"20570-8\"  ";
+    String code = codeSystem + "\t \n " + codeCode;
+    List<String> values = Helper.splitCode(code);
+    assertEquals(2, values.size());
+    assertEquals(codeSystem.trim(), values.get(0));
+    assertEquals(codeCode.trim(), values.get(1));
+  }
+
+  @Test
+  public void testTrimQuotes() {
+    String onlyQuotes = "\"\"";
+    assertEquals("", Helper.trimQuotes(onlyQuotes));
+    String normalQuotes = "\"abc\"";
+    assertEquals("abc", Helper.trimQuotes(normalQuotes));
+  }
+
+  @Test
+  public void testTrimCharacter() {
+    String emptySpaces = "     ";
+    assertEquals("", Helper.trimCharacter(emptySpaces, ' '));
+    String wrappingSpaces = "  abc   ";
+    assertEquals("abc", Helper.trimCharacter(wrappingSpaces, ' '));
+    String onlyRightSideSpaces = "ab ";
+    assertEquals("ab", Helper.trimCharacter(onlyRightSideSpaces, ' '));
+    String onlyLeftSideSpaces = " abc";
+    assertEquals("abc", Helper.trimCharacter(onlyLeftSideSpaces, ' '));
+    String noSpaces = "abc";
+    assertEquals("abc", Helper.trimCharacter(noSpaces, ' '));
+    String empty = "";
+    assertEquals("", Helper.trimCharacter(empty, ' '));
+    assertNull(Helper.trimCharacter(null, ' '));
+    String interspersedSpaces = " a b c ";
+    assertEquals("a b c", Helper.trimCharacter(interspersedSpaces, ' '));
+  }
+
   @Test
   public void testGetDateFromISO() throws ParseException {
     assertThrows(NullPointerException.class, () -> Helper.getDateFromISO(null));
