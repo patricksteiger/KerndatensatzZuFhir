9faa919d59544e8e274288ba0eeeb82c21bfe2fc
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-03-12 20:09:05 +0100

    add tests for UnitConverter

--- src/test/java/unit/converter/UnitConverterTest.java
+++ src/test/java/unit/converter/UnitConverterTest.java
@@ -8,31 +8,86 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-// TODO: More Tests for UnitConverter
 class UnitConverterTest {
   @Test
   void testEmpty() {
     assertFalse(UnitConverter.fromLocalCode(null, null).isPresent());
     assertFalse(UnitConverter.fromLocalCode("", "").isPresent());
+    assertFalse(UnitConverter.fromLocalCode(null, "").isPresent());
+    assertFalse(UnitConverter.fromLocalCode("", null).isPresent());
     String unit = "ml";
     assertFalse(UnitConverter.fromLocalCode(unit, "").isPresent());
     assertFalse(UnitConverter.fromLocalCode(unit, null).isPresent());
   }
 
   @Test
-  void testNonReducable() {
+  void testNonReducible() {
     String value = "120/80";
     String unit = "mm Hg";
     Optional<Quantity> result = UnitConverter.fromLocalCode(unit, value);
     assertQuantity(value, "mm[Hg]", result);
   }
 
+  @Test
+  void testInvalidUnit() {
+    String value = "1";
+    String unit = "invalid";
+    assertFalse(UnitConverter.fromLocalCode(unit, value).isPresent());
+  }
+
+  @Test
+  void testInvalidValue() {
+    String value = "invalid";
+    String unit = "mL";
+    assertFalse(UnitConverter.fromLocalCode(unit, value).isPresent());
+  }
+
+  @Test
+  void testSimpleValue() {
+    String value = "12";
+    String unit = "mL";
+    Optional<Quantity> result = UnitConverter.fromLocalCode(unit, value);
+    assertQuantity(value, unit, result);
+  }
+
+  @Test
+  void testFractionValue() {
+    String value = "1/2";
+    String unit = "mL";
+    Optional<Quantity> result = UnitConverter.fromLocalCode(unit, value);
+    assertQuantity("0.5", unit, result);
+  }
+
   private void assertQuantity(
       String expectedValue, String expectedUnit, Optional<Quantity> actualQuantity) {
     assertTrue(actualQuantity.isPresent());
     Quantity quantity = actualQuantity.get();
-    assertEquals(expectedValue, quantity.getValueElement().getValueAsString());
+    assertValueEquals(expectedValue, quantity.getValueElement().getValueAsString());
     assertEquals(expectedUnit, quantity.getCode());
     assertEquals(Constants.QUANTITY_SYSTEM, quantity.getSystem());
+  }
+
+  private void assertValueEquals(String expected, String actual) {
+    if (expected.length() != actual.length() && !expected.contains(".")) {
+      assertEquals(expected, actual);
+    }
+    int len = Math.min(expected.length(), actual.length());
+    // Check if shared indices are equal
+    for (int i = 0; i < len; i++) {
+      if (expected.charAt(i) != actual.charAt(i)) {
+        assertEquals(expected, actual);
+      }
+    }
+    // Check if excess indices are equal
+    for (int i = expected.length(); i < actual.length(); i++) {
+      if (actual.charAt(i) != '0') {
+        assertEquals(expected, actual);
+      }
+    }
+    for (int i = actual.length(); i < expected.length(); i++) {
+      if (expected.charAt(i) != '0') {
+        assertEquals(expected, actual);
+      }
+    }
   }
 }
