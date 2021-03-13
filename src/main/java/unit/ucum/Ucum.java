43e2e289ac679bb20c6b8d3656aa0f46d459ecbc
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-03-13 20:33:02 +0100

    add documentation to Ucum

--- src/main/java/unit/ucum/Ucum.java
+++ src/main/java/unit/ucum/Ucum.java
@@ -19,10 +19,22 @@ public class Ucum {
 
   private Ucum() {}
 
+  /**
+   * Checks whether the given unit is in UCUM-Format.
+   *
+   * @param unit Unit to be checked
+   * @return true if unit is in UCUM-Format, false otherwise
+   */
   public static boolean validate(String unit) {
     return unit != null && service.validate(unit) == null;
   }
 
+  /**
+   * Returns representation of UCUM-Unit generally used for "unit" in FHIR-Resource "Quantity".
+   *
+   * @param ucumUnit UCUM-Unit
+   * @return Representation of UCUM-Unit, returns empty if given unit is not int UCUM-Format
+   */
   public static Optional<String> formalRepresentation(String ucumUnit) {
     try {
       String formal = service.analyse(ucumUnit);
