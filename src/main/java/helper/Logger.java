d88fcb1c9dce3650144f0c572ffedaf10fbec9b0
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-03-24 22:15:29 +0100

    refactor Logger to count errors correctly and rewrite messages

--- src/main/java/helper/Logger.java
+++ src/main/java/helper/Logger.java
@@ -8,6 +8,7 @@ import java.util.function.Supplier;
 
 public class Logger {
 
+  private static final String ERROR_IN_MEITHOD = "An error occurred in method: {}\n";
   private final org.slf4j.Logger SL4J_LOGGER;
   private long errorCounter;
 
@@ -17,11 +18,10 @@ public class Logger {
   }
 
   public <T> Supplier<T> error(String method, String valueName, String value) {
-    countError();
     return () -> {
+      countError();
       this.SL4J_LOGGER.error(
-          "In method \"{}\" an error occurred!\n"
-              + "Datablock-Valuename: \"{}\", invalid value: \"{}\".\n",
+          ERROR_IN_MEITHOD + "Datablock-Value \"{}\", invalid value: \"{}\".\n",
           method,
           valueName,
           value);
@@ -30,28 +30,25 @@ public class Logger {
   }
 
   public <T> Supplier<T> error(String method, String message) {
-    countError();
     return () -> {
-      this.SL4J_LOGGER.error(
-          "In method \"{}\" an error occurred!\n" + "Message: {}", method, message);
+      countError();
+      this.SL4J_LOGGER.error(ERROR_IN_MEITHOD + "Message: {}", method, message);
       return Constants.getEmptyValue();
     };
   }
 
   public <T> Supplier<T> emptyValue(String method, String value) {
-    countError();
     return () -> {
+      countError();
       this.SL4J_LOGGER.error(
-          "In method \"{}\" an error occurred!\n" + "Datablock-Value \"{}\" has to have a value!",
-          method,
-          value);
+          ERROR_IN_MEITHOD + "Datablock-Value \"{}\" has to have a value!\n", method, value);
       return Constants.getEmptyValue();
     };
   }
 
   public <T> Supplier<T> warn(T value, String method, String message) {
     return () -> {
-      this.SL4J_LOGGER.warn("In method {} a warning occured!\n" + "Message: {}", method, message);
+      this.SL4J_LOGGER.warn("A warning occured in method: {}\n" + "Message: {}\n", method, message);
       return value;
     };
   }
