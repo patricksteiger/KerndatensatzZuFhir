6c9c2e4f5d2cb3f251b7652533bb6ff83d6d81e8
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-04-09 17:17:03 +0200

    remove unnecessary reference to specimen

--- src/main/java/constants/MIIReference.java
+++ src/main/java/constants/MIIReference.java
@@ -3,8 +3,8 @@ package constants;
 import enums.MIICoreLocations;
 
 public class MIIReference {
-  public static final String ORGANIZATION = "Organization/";
-  public static final String MII_ORGANIZATION = ORGANIZATION + MIICoreLocations.UKU.toString();
+  private static final String ORGANIZATION = "Organization/";
+  public static final String MII_ORGANIZATION = ORGANIZATION + MIICoreLocations.UKU;
   private static final String PATIENT = "Patient/";
   public static final String MII_PATIENT = PATIENT + "MII-Patient";
   private static final String CONSENT = "Consent/";
@@ -15,8 +15,6 @@ public class MIIReference {
   public static final String MII_SERVICE_REQUEST = SERVICE_REQUEST + "MII-ServiceRequest";
   private static final String MEDICATION = "Medication/";
   public static final String REF_MEDICATION = MEDICATION + "Reference-Medication";
-  private static final String SPECIMEN = "Specimen/";
-  public static final String REF_SPECIMEN = SPECIMEN + "Specimen";
 
   private MIIReference() {}
 }
