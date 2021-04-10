9d7d6cdca1d9eab5d679bad86480f2b5d3463ae2
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-04-10 21:13:39 +0200

    add documentation to ParsedRatio

--- src/main/java/helper/ParsedRatio.java
+++ src/main/java/helper/ParsedRatio.java
@@ -8,6 +8,13 @@ import java.util.Optional;
 public class ParsedRatio {
   private ParsedRatio() {}
 
+  /**
+   * Parses Ratio from a String formatted like "value="1/2" unit="mmol/L"". If no denominator is
+   * given (e.g. "value="1" unit="g"") it is set to 1 for both value and unit.
+   *
+   * @param str Expected format: "value="1/2" unit="mmol/L""
+   * @return Ratio, if value and unit are valid. Otherwise empty.
+   */
   public static Optional<Ratio> fromString(String str) {
     List<String> words = Helper.splitCode(str);
     String value = Helper.extractCode(words, "value=");
