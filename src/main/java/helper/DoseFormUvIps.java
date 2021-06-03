91d0701954bc6b46f37250cee5afce8edbb95f23
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-06-03 22:32:44 +0200

    add the rest of codes starting with 1 to validation

--- src/main/java/helper/DoseFormUvIps.java
+++ src/main/java/helper/DoseFormUvIps.java
@@ -20,69 +20,249 @@ public class DoseFormUvIps implements Code {
   }
 
   public static boolean validDoseFormUvIpsCode(String code) {
-    return isFormCode101(code)
-        || isFormCode102(code)
-        || isFormCode103(code)
-        || isFormCode104(code)
-        || isFormCode105(code)
-        || isFormCode106(code)
-        || isFormCode107(code)
-        || isFormCode108(code)
-        || isFormCode109(code)
-        || isFormCode110(code);
+    if (Helper.checkEmptyString(code) || code.length() < 5 || code.length() > 8) {
+      return false;
+    }
+    char n = code.charAt(0);
+    switch (n) {
+      case '1':
+        return isFormCode1(code);
+        // TODO: '2' and rest
+      default:
+        return false;
+    }
+  }
+
+  private static boolean isFormCode1(String code) {
+    char n = code.charAt(1);
+    switch (n) {
+      case '0':
+        return isFormCode10(code);
+      case '1':
+        return isFormCode11(code);
+      case '2':
+        return isFormCode12(code);
+      case '3':
+        return isFormCode13(code);
+      case '4':
+        return isFormCode14(code);
+      case '5':
+        return isFormCode15(code);
+      case '6':
+        return isFormCode16(code);
+      case '7':
+        return isFormCode17(code);
+      case '8':
+        return isFormCode18(code);
+      case '9':
+        return isFormCode19(code);
+      default:
+        return false;
+    }
+  }
+
+  private static boolean isFormCode19(String code) {
+    return "19050".equals(code) || "19100".equals(code);
+  }
+
+  private static boolean isFormCode18(String code) {
+    return "18040".equals(code) || "18080".equals(code);
+  }
+
+  private static boolean isFormCode17(String code) {
+    return "17040".equals(code) || "17090".equals(code) || "17120".equals(code);
+  }
+
+  private static boolean isFormCode16(String code) {
+    return "16040".equals(code);
+  }
+
+  private static boolean isFormCode15(String code) {
+    return "15090".equals(code) || "15130".equals(code);
+  }
+
+  private static boolean isFormCode14(String code) {
+    return "14050".equals(code);
+  }
+
+  private static boolean isFormCode13(String code) {
+    return "13050".equals(code) || "13220".equals(code);
+  }
+
+  private static boolean isFormCode12(String code) {
+    char c = code.charAt(2);
+    switch (c) {
+      case '0':
+        return isFormCode120(code);
+      case '1':
+        return isFormCode121(code);
+      case '2':
+        return isFormCode122(code);
+      case '3':
+        return isFormCode123(code);
+      default:
+        return false;
+    }
+  }
+
+  private static boolean isFormCode123(String code) {
+    return isFormCode(code, 1, 3);
+  }
+
+  private static boolean isFormCode122(String code) {
+    return "12200".equals(code);
+  }
+
+  private static boolean isFormCode121(String code) {
+    return isFormCode(code, 1, 20)
+        || isFormCode(code, 30, 31)
+        || "12100".equals(code)
+        || "12100500".equals(code)
+        || "12109500".equals(code)
+        || "12115500".equals(code)
+        || "12120".equals(code)
+        || "12150".equals(code);
+  }
+
+  private static boolean isFormCode120(String code) {
+    return "12004000".equals(code);
+  }
+
+  private static boolean isFormCode11(String code) {
+    char c = code.charAt(2);
+    switch (c) {
+      case '0':
+        return isFormCode110(code);
+      case '1':
+        return isFormCode111(code);
+      case '2':
+        return isFormCode112(code);
+      case '3':
+        return isFormCode113(code);
+      case '4':
+        return isFormCode114(code);
+      case '5':
+        return isFormCode115(code);
+      case '6':
+        return isFormCode116(code);
+      case '7':
+        return isFormCode117(code);
+      case '9':
+        return isFormCode119(code);
+      default:
+        return false;
+    }
+  }
+
+  private static boolean isFormCode119(String code) {
+    return isFormCode(code, 1, 6);
+  }
+
+  private static boolean isFormCode117(String code) {
+    return isFormCode(code, 1, 2);
+  }
+
+  private static boolean isFormCode116(String code) {
+    return isFormCode(code, 1, 4);
+  }
+
+  private static boolean isFormCode115(String code) {
+    return isFormCode(code, 1, 5);
+  }
+
+  private static boolean isFormCode114(String code) {
+    return isFormCode(code, 1, 5);
+  }
+
+  private static boolean isFormCode113(String code) {
+    return isFormCode(code, 1, 4) || "11303500".equals(code);
+  }
+
+  private static boolean isFormCode112(String code) {
+    return isFormCode(code, 1, 18) || "11210500".equals(code) || "11214500".equals(code);
+  }
+
+  private static boolean isFormCode111(String code) {
+    return isFormCode(code, 1, 18);
   }
 
   private static boolean isFormCode110(String code) {
-    return isFormCode(code, "110", 1, 15) || "11010".equals(code) || "11050".equals(code);
+    return isFormCode(code, 1, 15) || "11010".equals(code) || "11050".equals(code);
+  }
+
+  private static boolean isFormCode10(String code) {
+    char c = code.charAt(2);
+    switch (c) {
+      case '1':
+        return isFormCode101(code);
+      case '2':
+        return isFormCode102(code);
+      case '3':
+        return isFormCode103(code);
+      case '4':
+        return isFormCode104(code);
+      case '5':
+        return isFormCode105(code);
+      case '6':
+        return isFormCode106(code);
+      case '7':
+        return isFormCode107(code);
+      case '8':
+        return isFormCode108(code);
+      case '9':
+        return isFormCode109(code);
+      default:
+        return false;
+    }
   }
 
   private static boolean isFormCode109(String code) {
-    return isFormCode(code, "109", 1, 16) || "10900500".equals(code);
+    return isFormCode(code, 1, 16) || "10900500".equals(code);
   }
 
   private static boolean isFormCode108(String code) {
-    return isFormCode(code, "108", 1, 12);
+    return isFormCode(code, 1, 12);
   }
 
   private static boolean isFormCode107(String code) {
-    return isFormCode(code, "107", 1, 15);
+    return isFormCode(code, 1, 15);
   }
 
   private static boolean isFormCode106(String code) {
-    return isFormCode(code, "106", 1, 13) || "10604500".equals(code);
+    return isFormCode(code, 1, 13) || "10604500".equals(code);
   }
 
   private static boolean isFormCode105(String code) {
-    return isFormCode(code, "105", 1, 25)
+    return isFormCode(code, 1, 25)
         || "10517500".equals(code)
         || "10521500".equals(code)
         || "10539500".equals(code)
         || "10546500".equals(code)
-        || isFormCode(code, "105", 47, 50);
+        || isFormCode(code, 47, 50);
   }
 
   private static boolean isFormCode104(String code) {
-    return isFormCode(code, "104", 1, 11);
+    return isFormCode(code, 1, 11);
   }
 
   private static boolean isFormCode103(String code) {
-    return isFormCode(code, "103", 1, 23)
+    return isFormCode(code, 1, 23)
         || "10314005".equals(code)
         || "10314010".equals(code)
         || "10314011".equals(code);
   }
 
   private static boolean isFormCode102(String code) {
-    return isFormCode(code, "102", 1, 31) || "10236100".equals(code);
+    return isFormCode(code, 1, 31) || "10236100".equals(code);
   }
 
   private static boolean isFormCode101(String code) {
-    return isFormCode(code, "101", 1, 22);
+    return isFormCode(code, 1, 22);
   }
 
-  private static boolean isFormCode(String code, String start, int lo, int hi) {
+  private static boolean isFormCode(String code, int lo, int hi) {
     // Example-Code: 101xx000, 01 <= xx <= 22
-    if (code.length() != 8 || !code.startsWith(start) || !code.endsWith("000")) {
+    if (code.length() != 8 || !code.endsWith("000")) {
       return false;
     }
     String xx = code.substring(3, 5);
