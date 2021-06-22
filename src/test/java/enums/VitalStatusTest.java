c3616b9d4ed18e788e9f4b3d16e720a6202df993
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-06-22 19:56:51 +0200

    add tests for all valid codes and system

---
+++ src/test/java/enums/VitalStatusTest.java
@@ -0,0 +1,28 @@
+package enums;
+
+import org.junit.jupiter.api.Test;
+import valueSet.CodeDto;
+import valueSet.CodeUtil;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static valueSet.CodeUtil.assertSimpleSystem;
+import static valueSet.CodeUtil.assertValidCodes;
+
+class VitalStatusTest {
+  @Test
+  void testSystem() {
+    String expectedSystem =
+        "https://www.medizininformatik-initiative.de/fhir/core/modul-person/CodeSystem/Vitalstatus";
+    assertSimpleSystem(expectedSystem, VitalStatus.values());
+  }
+
+  @Test
+  void testAllValidCodes() throws FileNotFoundException {
+    List<CodeDto> codes = CodeUtil.get("VitalStatusCodes.json");
+    assertValidCodes(codes, VitalStatus::fromCode);
+    assertEquals(codes.size(), VitalStatus.values().length);
+  }
+}
