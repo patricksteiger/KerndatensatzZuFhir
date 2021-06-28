483b6579d6ffb6b8f4a92817f453af54dd63fd8e
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-06-28 17:20:29 +0200

    add tests for system and all valid codes

---
+++ src/test/java/enums/QuelleKlinischesBezugsdatumTest.java
@@ -0,0 +1,27 @@
+package enums;
+
+import org.junit.jupiter.api.Test;
+import valueSet.CodeDto;
+import valueSet.CodeUtil;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static valueSet.CodeUtil.assertSimpleSystem;
+import static valueSet.CodeUtil.assertValidCodes;
+
+class QuelleKlinischesBezugsdatumTest {
+  @Test
+  void testSystem() {
+    String expectedSystem = "http://snomed.info/sct";
+    assertSimpleSystem(expectedSystem, QuelleKlinischesBezugsdatum.values());
+  }
+
+  @Test
+  void testAllValidCodes() throws FileNotFoundException {
+    List<CodeDto> codes = CodeUtil.get("QuelleKlinischesBezugsdatumCodes.json");
+    assertValidCodes(codes, QuelleKlinischesBezugsdatum::fromCode);
+    assertEquals(codes.size(), QuelleKlinischesBezugsdatum.values().length);
+  }
+}
