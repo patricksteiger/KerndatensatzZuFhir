10b7d9180413f6677007276bfe6494b9d9cdd83f
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-06-28 17:37:12 +0200

    add tests for system and all valid codes

---
+++ src/test/java/enums/DurchfuehrungsabsichtCodeTest.java
@@ -0,0 +1,27 @@
+package enums;
+
+import org.junit.jupiter.api.Test;
+import valueSet.CodeDto;
+import valueSet.CodeUtil;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static valueSet.CodeUtil.assertSimpleSystem;
+import static valueSet.CodeUtil.assertValidCodes;
+
+class DurchfuehrungsabsichtCodeTest {
+  @Test
+  void testSystem() {
+    String expectedSystem = "http://snomed.info/sct";
+    assertSimpleSystem(expectedSystem, DurchfuehrungsabsichtCode.values());
+  }
+
+  @Test
+  void testAllValidCodes() throws FileNotFoundException {
+    List<CodeDto> codes = CodeUtil.get("DurchfuehrungsabsichtCodeCodes.json");
+    assertValidCodes(codes, DurchfuehrungsabsichtCode::fromCode);
+    assertEquals(codes.size(), DurchfuehrungsabsichtCode.values().length);
+  }
+}
