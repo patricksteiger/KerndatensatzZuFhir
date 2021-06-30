df587fc8ab83bc7dae2ce46525c73bbb70f2cbd9
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-06-30 19:44:41 +0200

    add test for system

--- src/test/java/enums/EntlassungsgrundTest.java
+++ src/test/java/enums/EntlassungsgrundTest.java
@@ -8,9 +8,17 @@ import java.io.FileNotFoundException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static valueSet.CodeUtil.assertSimpleSystem;
 import static valueSet.CodeUtil.assertValidCodes;
 
 class EntlassungsgrundTest {
+  @Test
+  void testSystem() {
+    String expectedSystem =
+        "https://www.medizininformatik-initiative.de/fhir/core/modul-fall/CodeSystem/Entlassungsgrund";
+    assertSimpleSystem(expectedSystem, Entlassungsgrund.values());
+  }
+
   @Test
   void testAllValidCodes() throws FileNotFoundException {
     List<CodeDto> codes = CodeUtil.get("EntlassungsgrundCodes.json");
