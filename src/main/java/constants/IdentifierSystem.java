a87b645a388794d5abeaa3b130769b98d4512102
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-07-09 18:43:16 +0200

    fix system of GKV and IKNR

--- src/main/java/constants/IdentifierSystem.java
+++ src/main/java/constants/IdentifierSystem.java
@@ -3,8 +3,8 @@ package constants;
 public class IdentifierSystem {
   public static final String EMPTY = null;
   public static final String LOCAL_PID = "http://uniklinik-ulm.de/fhir/NamingSystem/pid";
-  public static final String VERSICHERTEN_ID_GKV = "http://fhir.de/NamingSystem/gkv/kvid-10";
-  public static final String IKNR = "http://fhir.de/NamingSystem/arge-ik/iknr";
+  public static final String VERSICHERTEN_ID_GKV = "http://fhir.de/sid/gkv/kvid-10";
+  public static final String IKNR = "http://fhir.de/sid/arge-ik/iknr";
   public static final String ACME_PATIENT = "http://www.acme.com/identifiers/patient";
   public static final String NS_DIZ =
       "https://www.medizininformatik-initiative.de/fhir/core/NamingSystem/DIZ";
