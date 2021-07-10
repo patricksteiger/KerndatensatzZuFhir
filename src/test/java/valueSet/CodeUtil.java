751d3fe8842c0c544f87e1bac5c6031eb9fd3cd4
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-07-10 20:17:32 +0200

    add method to check all valid codes of a valueSet with the help of reflection

--- src/test/java/valueSet/CodeUtil.java
+++ src/test/java/valueSet/CodeUtil.java
@@ -7,17 +7,48 @@ import org.json.JSONTokener;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CodeUtil {
 
   public static final String RESOURCES_PATH = "src/test/resources/valueSet/";
+
+  /**
+   * To assert all valid codes this methods assumes a json-file in test/resources/valueSet named
+   * testedClassCodes.json exists, and on the root level exists the "codes"-field with all valid
+   * codes. Also the given class has to have a fromCode-Method expecting the code and returning an
+   * optional containing an instance of the testedClass. It also has to be be an Enum.
+   *
+   * @param testedClass
+   * @param <T>
+   * @param <Enum>
+   */
+  public static <T extends Code, Enum> void assertValidCodes(Class<T> testedClass) {
+    String className = testedClass.getSimpleName();
+    try {
+      Method fromCodeMethod = testedClass.getMethod("fromCode", String.class);
+      List<CodeDto> expectedCodes = get(className + "Codes.json");
+      for (CodeDto expectedCode : expectedCodes) {
+        Optional<T> code = (Optional<T>) fromCodeMethod.invoke(null, expectedCode.getCode());
+        assertTrue(code.isPresent(), "Expected code: \"" + expectedCode.getCode() + "\"");
+        T actualCode = code.get();
+        assertEquals(expectedCode.getCode(), actualCode.getCode());
+        assertEquals(expectedCode.getDisplay(), actualCode.getDisplay());
+      }
+      Method valuesMethod = testedClass.getMethod("values", null);
+      T[] values = (T[]) valuesMethod.invoke(null, null);
+      assertEquals(expectedCodes.size(), values.length);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail("Failed automated testing of all valid codes of valueSet: " + className);
+    }
+  }
 
   public static <T extends Code> void assertValidCodes(
       List<CodeDto> expectedCodes, Function<String, Optional<T>> actualCodeSupplier) {
