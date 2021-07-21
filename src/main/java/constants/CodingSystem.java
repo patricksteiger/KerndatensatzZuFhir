70e2f18076c4980e7a7dbb0fd65345a2f2d8da47
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-07-21 18:37:37 +0200

    add TODO for urn:oid-prefix

--- src/main/java/constants/CodingSystem.java
+++ src/main/java/constants/CodingSystem.java
@@ -3,6 +3,7 @@ package constants;
 public class CodingSystem {
   public static final String SNOMED_CLINICAL_TERMS = "http://snomed.info/sct";
   public static final String OPS_DIMDI = "http://fhir.de/CodeSystem/bfarm/ops";
+  // TODO: Is urn:oid - prefix needed for such systems?
   public static final String OPS_SEITENLOKALISATION = "urn:oid:2.16.840.1.113883.3.7.1.7";
   public static final String OPS_SEITENLOKALISATION_ERROR =
       "http://terminology.hl7.org/CodeSystem/v3-NullFlavor";
