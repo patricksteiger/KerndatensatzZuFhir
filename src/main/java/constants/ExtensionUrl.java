db7d45cbe1659f6dc691395afaf2d331f3deb387
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-09-04 20:10:56 +0200

    change RecordedDate to old URL

--- src/main/java/constants/ExtensionUrl.java
+++ src/main/java/constants/ExtensionUrl.java
@@ -4,7 +4,7 @@ public class ExtensionUrl {
   public static final String OPS_SEITENLOKALISATION =
       "http://fhir.de/StructureDefinition/seitenlokalisation";
   public static final String RECORDED_DATE =
-      "http://fhir.de/StructureDefinition/ProzedurDokumentationsdatum";
+      "https://www.medizininformatik-initiative.de/fhir/core/modul-prozedur/StructureDefinition/procedure-recordedDate";
   public static final String DURCHFUEHRUNGSABSICHT =
       "https://www.medizininformatik-initiative.de/fhir/core/modul-prozedur/StructureDefinition/Durchfuehrungsabsicht";
   public static final String NAMENSZUSATZ =
