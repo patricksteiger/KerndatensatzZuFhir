6c10bc96f4028a3a924fffe69c6335d805e974ca
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-09-17 19:25:45 +0200

    refacor NonReducibleUnit to ressemble Mapper

--- src/main/java/unit/converter/NonReducibleUcumUnit.java
+++ src/main/java/unit/converter/NonReducibleUcumUnit.java
@@ -4,15 +4,17 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class NonReducibleUcumUnit {
-  private static final Set<String> nonReducibleUnits = new HashSet<>();
-
-  static {
-    nonReducibleUnits.add("mm[Hg]");
-  }
+  private static final Set<String> nonReducibleUnits = generateNonReducibleUcumUnits();
 
   private NonReducibleUcumUnit() {}
 
-  public static boolean check(String unit) {
-    return nonReducibleUnits.contains(unit);
+  public static boolean check(String ucumUnit) {
+    return nonReducibleUnits.contains(ucumUnit);
+  }
+
+  public static Set<String> generateNonReducibleUcumUnits() {
+    Set<String> result = new HashSet<>();
+    result.add("mm[Hg]");
+    return result;
   }
 }
