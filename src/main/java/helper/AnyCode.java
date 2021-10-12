f6c30ade297022372385f3b645ba4dcede088adb
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-10-12 19:18:33 +0200

    add AnyCode and ParsedValueAndUnit

---
+++ src/main/java/helper/AnyCode.java
@@ -0,0 +1,51 @@
+package helper;
+
+import unit.mapping.LocalDisplayMapper;
+import unit.mapping.LoincMapping;
+import unit.mapping.Mapper;
+
+import static constants.CodingSystem.LOINC;
+
+public class AnyCode implements interfaces.Code {
+  private final String code;
+  private final String system;
+  private final String display;
+
+  private AnyCode(String code, String system, String display) {
+    this.code = code;
+    this.system = system;
+    this.display = display;
+  }
+
+  public static AnyCode fromLoincMapping(LoincMapping loincMapping) {
+    return new AnyCode(loincMapping.getCode(), LOINC, loincMapping.getDisplay());
+  }
+
+  // TODO: What is the system of local codes?
+  public static AnyCode fromLocalSystem(ParsedCode localCode) {
+    return LocalDisplayMapper.fromLocalCode(localCode.getCode())
+        .map(display -> new AnyCode(localCode.getCode(), "", display))
+        .orElseGet(() -> new AnyCode(localCode.getCode(), LOINC, localCode.getDisplay()));
+  }
+
+  public static AnyCode fromLocalCodeToLoinc(ParsedCode parsedCode, String localUnit) {
+    return Mapper.getLoincMappingFromLocalCodeAndUnit(parsedCode.getCode(), localUnit)
+        .map(AnyCode::fromLoincMapping)
+        .orElseGet(() -> AnyCode.fromLocalSystem(parsedCode));
+  }
+
+  @Override
+  public String getCode() {
+    return code;
+  }
+
+  @Override
+  public String getSystem() {
+    return system;
+  }
+
+  @Override
+  public String getDisplay() {
+    return display;
+  }
+}
---
+++ src/main/java/helper/ParsedValueAndUnit.java
@@ -0,0 +1,29 @@
+package helper;
+
+import java.util.List;
+
+public class ParsedValueAndUnit {
+
+  private final String value;
+  private final String unit;
+
+  private ParsedValueAndUnit(String value, String unit) {
+    this.value = value;
+    this.unit = unit;
+  }
+
+  public static ParsedValueAndUnit fromString(String s) {
+    List<String> splitCode = Helper.splitCode(s);
+    String value = Helper.extractCode(splitCode, "value=");
+    String unit = Helper.extractCode(splitCode, "unit=");
+    return new ParsedValueAndUnit(value, unit);
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public String getUnit() {
+    return unit;
+  }
+}
