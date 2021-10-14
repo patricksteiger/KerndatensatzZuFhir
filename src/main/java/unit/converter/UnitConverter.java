f1a7e20a89d64162c6a39eebd9c6d3fd33168448
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-10-14 18:05:56 +0200

    refactor UnitConverter to simplify fractions of local codes

--- src/main/java/unit/converter/UnitConverter.java
+++ src/main/java/unit/converter/UnitConverter.java
@@ -1,6 +1,5 @@
 package unit.converter;
 
-import constants.Constants;
 import helper.FhirGenerator;
 import helper.Helper;
 import org.hl7.fhir.r4.model.Quantity;
@@ -11,6 +10,8 @@ import unit.ucum.Ucum;
 
 import java.math.BigDecimal;
 import java.util.Optional;
+
+import static constants.Constants.QUANTITY_SYSTEM;
 
 public class UnitConverter {
 
@@ -34,44 +35,49 @@ public class UnitConverter {
         Mapper.getLoincMappingFromLocalCodeAndUnit(localCode, localUnit);
     return loincMapping.isPresent()
         ? quantityFromLoincMappingAndValue(loincMapping.get(), value)
-        : Optional.of(quantityFromLocalUnitAndValue(localUnit, value));
+        : quantityFromLocalUnitAndValue(localUnit, value);
   }
 
   // TODO: Reduce possible fractions
-  private static Quantity quantityFromLocalUnitAndValue(String localUnit, String value) {
-    boolean localUnitIsUcumUnit = Ucum.validate(localUnit);
-    String display = localUnitIsUcumUnit ? Ucum.formalRepresentation(localUnit).orElse("") : "";
-    String system = localUnitIsUcumUnit ? Constants.QUANTITY_SYSTEM : "";
-    return FhirGenerator.quantity(value, display, system, localUnit);
+  private static Optional<Quantity> quantityFromLocalUnitAndValue(String localUnit, String value) {
+    String system = Ucum.validate(localUnit) ? QUANTITY_SYSTEM : "";
+    return quantityFromValueUnitConversionWithSystem(value, localUnit, BigDecimal.ONE, system);
   }
 
   private static Optional<Quantity> quantityFromLoincMappingAndValue(
       LoincMapping loincMapping, String value) {
-    String ucumCode = loincMapping.getUcumCode();
-    return IrreducibleUnit.check(ucumCode)
-        ? quantity(value, loincMapping.getUcumCode())
-        : convertValue(value, loincMapping.getConversion()).flatMap(val -> quantity(val, ucumCode));
+    String unit = loincMapping.getUcumCode();
+    BigDecimal conversion = loincMapping.getConversion();
+    return quantityFromValueUnitConversionWithSystem(value, unit, conversion, QUANTITY_SYSTEM);
   }
 
   private static Optional<Quantity> quantityFromUnitMappingAndValue(
       UnitMapping mapping, String value) {
-    String ucumCode = mapping.getUcumCode();
-    return IrreducibleUnit.check(ucumCode)
-        ? quantity(value, mapping.getUcumCode())
-        : convertValue(value, mapping.getConversion()).flatMap(val -> quantity(val, ucumCode));
+    String unit = mapping.getUcumCode();
+    BigDecimal conversion = mapping.getConversion();
+    return quantityFromValueUnitConversionWithSystem(value, unit, conversion, QUANTITY_SYSTEM);
+  }
+
+  private static Optional<Quantity> quantityFromValueUnitConversionWithSystem(
+      String value, String unit, BigDecimal conversion, String system) {
+    return IrreducibleUnit.check(unit)
+        ? quantity(value, unit, system)
+        : convertValue(value, conversion).flatMap(val -> quantity(val, unit, system));
   }
 
   private static Optional<BigDecimal> convertValue(String value, BigDecimal conversion) {
     return Helper.parseBigDecimalFromQuantity(value).map(conversion::multiply);
   }
 
-  private static Optional<Quantity> quantity(BigDecimal value, String ucumCode) {
-    return Ucum.formalRepresentation(ucumCode)
-        .map(unit -> FhirGenerator.quantity(value, unit, Constants.QUANTITY_SYSTEM, ucumCode));
+  private static Optional<Quantity> quantity(BigDecimal value, String unit, String system) {
+    return QUANTITY_SYSTEM.equals(system)
+        ? Ucum.formalRepresentation(unit).map(r -> FhirGenerator.quantity(value, r, system, unit))
+        : Optional.of(FhirGenerator.quantity(value, unit, system, unit));
   }
 
-  private static Optional<Quantity> quantity(String value, String ucumCode) {
-    return Ucum.formalRepresentation(ucumCode)
-        .map(unit -> FhirGenerator.quantity(value, unit, Constants.QUANTITY_SYSTEM, ucumCode));
+  private static Optional<Quantity> quantity(String value, String unit, String system) {
+    return QUANTITY_SYSTEM.equals(system)
+        ? Ucum.formalRepresentation(unit).map(r -> FhirGenerator.quantity(value, r, system, unit))
+        : Optional.of(FhirGenerator.quantity(value, unit, system, unit));
   }
 }
