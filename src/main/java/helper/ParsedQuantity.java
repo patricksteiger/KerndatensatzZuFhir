bd9788d98ce2d74347b00b11e5f920f9e822d6f2
Patrick Steiger <patrick.steiger@uni-ulm.de>
2021-10-15 18:20:51 +0200

    add ParsedValueAndUnit to ParsedQuantity

--- src/main/java/helper/ParsedQuantity.java
+++ src/main/java/helper/ParsedQuantity.java
@@ -3,7 +3,6 @@ package helper;
 import org.hl7.fhir.r4.model.Quantity;
 import unit.converter.UnitConverter;
 
-import java.util.List;
 import java.util.Optional;
 
 public class ParsedQuantity {
@@ -11,17 +10,13 @@ public class ParsedQuantity {
   private ParsedQuantity() {}
 
   public static Optional<Quantity> fromString(String s) {
-    List<String> splitCode = Helper.splitCode(s);
-    String value = Helper.extractCode(splitCode, "value=");
-    String unit = Helper.extractCode(splitCode, "unit=");
-    return UnitConverter.fromLocalUnit(unit, value);
+    var parsedValue = ParsedValueAndUnit.fromString(s);
+    return UnitConverter.fromLocalUnit(parsedValue.getUnit(), parsedValue.getValue());
   }
 
   public static Optional<Quantity> fromStringWithLocalCode(String s, String localCode) {
-    List<String> splitCode = Helper.splitCode(s);
-    String value = Helper.extractCode(splitCode, "value=");
-    String unit = Helper.extractCode(splitCode, "unit=");
+    var parsedValue = ParsedValueAndUnit.fromString(s);
     String code = ParsedCode.fromString(localCode).getCode();
-    return UnitConverter.fromLocalCodeAndUnit(code, unit, value);
+    return UnitConverter.fromLocalCodeAndUnit(code, parsedValue.getUnit(), parsedValue.getValue());
   }
 }
